c779e4083a63c19aca7b16bf37ce20b1f78c7616
Saiteja <91063931+ParsiSaiteja@users.noreply.github.com>
2021-12-01 18:42:34 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -6,7 +6,7 @@ pdfReader = PyPDF4.PdfFileReader(book)
 pages = pdfReader.numPages
 print(pages)
 speaker = pyttsx3.init()
-for num in range(1,110):
+for num in range(1,100):
  page = pdfReader.getPage(num)
  text = page.extractText()
  speaker.say(text)
